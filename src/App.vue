a62023ea78aa6f56d542223d685a81113c76495f
Clare <37682901+2oops@users.noreply.github.com>
2018-07-01 22:35:24 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img src="./assets/images/logo.png">
+    <first></first>
+  </div>
+</template>
+
+<script>
+import first from './components/first'
+export default {
+  data(){
+    return{}
+  },
+  components:{
+    first
+  }
+}
+</script>
+
+<style>
+@import 'assets/css/app.css'
+</style>
